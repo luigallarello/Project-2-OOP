cc890d65c1017cbd4dd047862e559dc1e0382f41
luigallarello <a1922789@adelaide.edu.au>
2026-09-02 11:23:05 +0930

    undead properties

--- undead.py
+++ undead.py
@@ -4,7 +4,7 @@ class Undead:
     MIN_HEALTH = 0
     MAX_HEALTH = 100
     MIN_POWER = 1
-    MAX_POWER = 100
+    MAX_POWER = 200
 
     HEALTH_PER_LEVEL = 2
     POWER_PER_LEVEL = 4
@@ -19,14 +19,41 @@ class Undead:
     def get_unit_id(self):
         return self.__unit_id
 
+    unit_id = property(get_unit_id)
+
     def get_name(self):
         return self.__name
+
+    name = property(get_name)
 
     def get_health(self):
         return self.__health
 
+    health = property(get_health)
+
     def get_power(self):
         return self.__power
 
+    power = property(get_power)
+
     def get_level(self):
         return self.__level
+
+    level = property(get_level)
+
+    def level_up(self):
+        if self.__level >= self.MAX_LEVEL:
+            print('already at max level')
+            return False
+
+        self__level += 1
+        self.__health = min(self.__health + self.HEALTH_PER_LEVEL, self.MAX_HEALTH)
+        self.__power = min(self.__power + self.POWER_PER_LEVEL, self.MAX_POWER)
+        return True
+
+    def __str__(self):
+        return (f'Unit id = {self.unit_id}\n'
+                f'Name = {self.name}\n'
+                f'Health = {self.health}'
+                f'Power = {self.power}'
+                f'Level = {self.level}')
